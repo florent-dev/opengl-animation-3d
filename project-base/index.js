4c6858a2d60840152b285ded63eba78eb1df3a77
Florent <florent84@live.fr>
2020-04-02 12:32:47 +0200

    début et fin d'une partie

--- project-base/index.js
+++ project-base/index.js
@@ -1,15 +1,4 @@
-window.onload = function() {
-    document.getElementById("page").style.visibility='hidden';
-    document.getElementById("fin").style.visibility='hidden';
-}
-
 function start(){
-    document.getElementById("debut").style.visibility='hidden';
     document.getElementById("page").style.visibility='visible';
     webGLStart();
-}
-
-function gameOver(){
-    document.getElementById("page").style.visibility='hidden';
-    document.getElementById("fin").style.visibility='visible';
 }
